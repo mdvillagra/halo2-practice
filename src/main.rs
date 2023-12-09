50e84b9c599f0160b61be903ce9931410a6b5349
Marcos Villagra <mdvillagra@gmail.com>
2023-12-09 19:22:53 -0300

    synthesize

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ use halo2_proofs::{
     plonk::{Advice, Assigned, Circuit, Column, ConstraintSystem, Error, Fixed, Instance},
     poly::Rotation,
 };
+use std::default;
 use std::marker::PhantomData;
 
 /// Configuration of columns
 /// l*sl + r*sr + (l*r)*sm - o*so + sc + PI = 0
-struct Config {
+struct CustomConfig {
     l: Column<Advice>,
     r: Column<Advice>,
     o: Column<Advice>,
@@ -24,12 +25,12 @@ struct Config {
 }
 
 struct CustomChip<F: FieldExt> {
-    config: Config,
+    config: CustomConfig,
     marker: PhantomData<F>,
 }
 
 impl<F: FieldExt> CustomChip<F> {
-    fn new(config: Config) -> CustomChip<F> {
+    fn new(config: CustomConfig) -> CustomChip<F> {
         CustomChip {
             config: config,
             marker: Default::default(),
@@ -168,6 +169,89 @@ impl<F: FieldExt> Composer<F> for CustomChip<F> {
         row: usize,
     ) -> Result<(), Error> {
         layouter.constrain_instance(cell, self.config.PI, row)
+    }
+}
+
+#[derive(Default)]
+struct SampleCircuit<F: FieldExt> {
+    x: Value<F>,
+    y: Value<F>,
+    constant: F,
+}
+
+impl<F: FieldExt> Circuit for SampleCircuit<F> {
+    type Config = CustomConfig;
+    type FloorPlanner = SimpleFloorPlanner;
+
+    fn without_witnesses(&self) -> Self {
+        Self::default()
+    }
+
+    fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+        let l = meta.advice_column();
+        let r = meta.advice_column();
+        let o = meta.advice_column();
+
+        meta.enable_equality(l);
+        meta.enable_equality(r);
+        meta.enable_equality(o);
+
+        let sm = meta.fixed_column();
+        let sl = meta.fixed_column();
+        let sr = meta.fixed_column();
+        let so = meta.fixed_column();
+        let sc = meta.fixed_column();
+        let sp = meta.fixed_column();
+
+        #[allow(non_snake_case)]
+        let PI = meta.instance_column();
+        meta.enable_equality(PI);
+
+        meta.create_gate("mini plonk", |meta| {
+            let l = meta.query_advice(l, Rotation::cur());
+            let r = meta.query_advice(r, Rotation::cur());
+            let o = meta.query_advice(o, Rotation::cur());
+
+            let sl = meta.query_advice(sl, Rotation::cur());
+            let sr = meta.query_advice(sr, Rotation::cur());
+            let so = meta.query_advice(so, Rotation::cur());
+            let sm = meta.query_advice(sm, Rotation::cur());
+            let sc = meta.query_advice(sc, Rotation::cur());
+
+            vec![l.clone() * sl + r.clone() * sr + l * r * sm + (o * so * (-F::one())) + sc]
+        });
+
+        meta.create_gate("public input", |meta| {
+            let l = meta.query_fixed(l, Rotation::cur());
+            #[allow(non_snake_case)]
+            let PI = meta.query_fixed(PI, Rotation::cur());
+            let sp = meta.query_fixed(sp, Rotation::cur());
+
+            vec![sp * (l - PI)]
+        });
+
+        CustomConfig {
+            l,
+            r,
+            o,
+            sl,
+            sr,
+            so,
+            sm,
+            sc,
+            PI,
+        }
+    }
+
+    fn synthesize(&self, config: Self::Config, layouter: impl Layouter<F>) -> Result<(), Error> {
+        let cs = CustomChip::new(config);
+
+        let x = self.x.into();
+        let y = self.y.into();
+        let consty = Assigned::from(self.constant);
+
+        let (a0, b0, c0) = cs.raw_multiply(&mut layouter, || x.map(|x| (x, x, x * x)))?;
+        cs.copy(&mut layouter, a0, b0)
     }
 }
 
