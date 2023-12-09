928e9145b803e6d131b0660edc4e3ad541347e7c
Marcos Villagra <mdvillagra@gmail.com>
2023-12-09 17:22:55 -0300

    composer

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use halo2_proofs::{
 use std::marker::PhantomData;
 
 /// Configuration of columns
-/// l*sl + r*sr + (l*r)*sm - o*s0 + sc + PI = 0
+/// l*sl + r*sr + (l*r)*sm - o*so + sc + PI = 0
 struct Config {
     l: Column<Advice>,
     r: Column<Advice>,
@@ -19,7 +19,8 @@ struct Config {
     so: Column<Fixed>,
     sm: Column<Fixed>,
     sc: Column<Fixed>,
-    PI: Column<Fixed>,
+
+    PI: Column<Instance>,
 }
 
 struct CustomChip<F: FieldExt> {
@@ -40,7 +41,7 @@ trait Composer<F: FieldExt> {
     fn raw_add<FM>(
         &self,
         layouter: &mut impl Layouter<F>,
-        f: F,
+        f: FM,
     ) -> Result<(Cell, Cell, Cell), Error>
     where
         FM: FnMut() -> Value<(Assigned<F>, Assigned<F>, Assigned<F>)>;
@@ -48,14 +49,126 @@ trait Composer<F: FieldExt> {
     fn raw_multiply<FM>(
         &self,
         layouter: &mut impl Layouter<F>,
-        f: F,
+        f: FM,
     ) -> Result<(Cell, Cell, Cell), Error>
     where
         FM: FnMut() -> Value<(Assigned<F>, Assigned<F>, Assigned<F>)>;
 
     fn copy(&self, layouter: &mut impl Layouter<F>, a: Cell, b: Cell) -> Result<(), Error>;
-    
-    fn expose_public(&self, layouter: &mut impl Layouter<F>, cell: Cell, row: usize);
+
+    fn expose_public(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        cell: Cell,
+        row: usize,
+    ) -> Result<(), Error>;
+}
+
+impl<F: FieldExt> Composer<F> for CustomChip<F> {
+    fn raw_multiply<FM>(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        mut f: FM,
+    ) -> Result<(Cell, Cell, Cell), Error>
+    where
+        FM: FnMut() -> Value<(Assigned<F>, Assigned<F>, Assigned<F>)>,
+    {
+        layouter.assign_region(
+            || "mul",
+            |mut region| {
+                let mut values = None;
+                let lhs = region.assign_advice(
+                    || "lhs",
+                    self.config.l,
+                    0,
+                    || {
+                        values = Some(f());
+                        values.unwrap().map(|v| v.0)
+                    },
+                )?;
+                let rhs = region.assign_advice(
+                    || "rhs",
+                    self.config.r,
+                    0,
+                    || values.unwrap().map(|v| v.1),
+                )?;
+
+                let out = region.assign_advice(
+                    || "out",
+                    self.config.o,
+                    0,
+                    || values.unwrap().map(|v| v.2),
+                )?;
+
+                region.assign_fixed(|| "m", self.config.sm, 0, || Value::known(F::one()))?;
+                region.assign_fixed(|| "o", self.config.so, 0, || Value::known(F::one()))?;
+
+                Ok((lhs.cell(), rhs.cell(), out.cell()))
+            },
+        )
+    }
+
+    fn raw_add<FM>(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        mut f: FM,
+    ) -> Result<(Cell, Cell, Cell), Error>
+    where
+        FM: FnMut() -> Value<(Assigned<F>, Assigned<F>, Assigned<F>)>,
+    {
+        layouter.assign_region(
+            || "add",
+            |mut region| {
+                let mut values = None;
+                let lhs = region.assign_advice(
+                    || "lhs",
+                    self.config.l,
+                    0,
+                    || {
+                        values = Some(f());
+                        values.unwrap().map(|v| v.0)
+                    },
+                )?;
+                let rhs = region.assign_advice(
+                    || "rhs",
+                    self.config.l,
+                    0,
+                    || values.unwrap().map(|v| v.0),
+                )?;
+                let out = region.assign_advice(
+                    || "out",
+                    self.config.l,
+                    0,
+                    || values.unwrap().map(|v| v.0),
+                )?;
+
+                region.assign_fixed(|| "l", self.config.sl, 0, || Value::known(F::one()))?;
+                region.assign_fixed(|| "r", self.config.sr, 0, || Value::known(F::one()))?;
+                region.assign_fixed(|| "o", self.config.so, 0, || Value::known(F::one()))?;
+
+                Ok((lhs.cell(), rhs.cell(), out.cell()))
+            },
+        )
+    }
+
+    fn copy(&self, layouter: &mut impl Layouter<F>, a: Cell, b: Cell) -> Result<(), Error> {
+        layouter.assign_region(
+            || "copy",
+            |mut region| {
+                region.constrain_equal(a, b)?;
+                region.constrain_equal(a, b)
+            },
+        )
+    }
+
+    fn expose_public(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        cell: Cell,
+        row: usize,
+    ) -> Result<(), Error> {
+        layouter.constrain_instance(cell, self.config.PI, row)
+    }
 }
 
 fn main() {
